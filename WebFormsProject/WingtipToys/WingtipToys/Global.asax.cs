b68f5e154b8b2bd98fa4e6ed0f9d6a81be4bb34b
martjd31 <martjd31@verizon.net>
2014-05-11 21:51:14 -0400

    error reporting

--- WebFormsProject/WingtipToys/WingtipToys/Global.asax.cs
+++ WebFormsProject/WingtipToys/WingtipToys/Global.asax.cs
@@ -23,9 +23,43 @@ namespace WingtipToys
             // Initialize the product database.
             Database.SetInitializer(new ProductDatabaseInitializer());
 
-            // Create the administrator role and user.
+            // Create administrator role and user.
             RoleActions roleActions = new RoleActions();
             roleActions.createAdmin();
+
+            // Add Routes.
+            RegisterCustomRoutes(RouteTable.Routes);
+        }
+
+        void RegisterCustomRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "ProductsByCategoryRoute",
+                "Category/{categoryName}",
+                "~/ProductList.aspx"
+            );
+            routes.MapPageRoute(
+                "ProductByNameRoute",
+                "Product/{productName}",
+                "~/ProductDetails.aspx"
+            );
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs.
+
+            // Get last error from the server
+            Exception exc = Server.GetLastError();
+
+            if (exc is HttpUnhandledException)
+            {
+                if (exc.InnerException != null)
+                {
+                    exc = new Exception(exc.InnerException.Message);
+                    Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
+                        true);
+                }
+            }
         }
     }
 }
